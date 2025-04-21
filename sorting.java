9e5c27ab57c256b4ac505cb956ea0e37b3d42b49
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2025-04-21 23:21:19 +0530

    Remove commented-out selection sort implementation and clean up insertion sort code

--- sorting.java
+++ sorting.java
@@ -25,16 +25,30 @@ public class sorting {
         // printArr(arr);
 
         // Selection sort
-        for(int i = 0; i < arr.length-1; i++){
-            int smallest = i;
-            for(int j = i+1; j < arr.length; j++){
-                if(arr[smallest] > arr[j]){
-                    smallest = j;
-                }
+        // for(int i = 0; i < arr.length-1; i++){
+        //     int smallest = i;
+        //     for(int j = i+1; j < arr.length; j++){
+        //         if(arr[smallest] > arr[j]){
+        //             smallest = j;
+        //         }
+        //     }
+        //     int temp = arr[smallest];
+        //     arr[smallest] = arr[i];
+        //     arr[i] = temp;
+        // }
+        // printArr(arr);
+
+        // insertion sort
+        for(int i =1; i < arr.length; i++){
+            int current = arr[i];
+            int j = i - 1;
+
+            while(j >= 0 && current < arr[j]){
+                // keep swaping
+                arr[j + 1] = arr[j];
+                j--;
             }
-            int temp = arr[smallest];
-            arr[smallest] = arr[i];
-            arr[i] = temp;
+            arr[j+ 1] = current;
         }
         printArr(arr);
     }
