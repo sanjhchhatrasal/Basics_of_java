5bf469acff7f886a8e84570cc6cbcbebd429ff91
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2025-04-21 22:56:52 +0530

    Add sorting implementation with selection sort and printArr method

---
+++ sorting.java
@@ -0,0 +1,41 @@
+public class sorting {
+
+    public static void printArr(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args){
+        int arr[] = {7, 8, 3, 1, 2};
+
+        // time complexity = o(n^2)
+        // Bubble sort
+        // for(int i = 0; i < arr.length-1; i++){
+        //     for(int j = 0; j < arr.length-i-1; j++){
+        //         if(arr[j] > arr[j+1]){
+        //             // swap
+        //             int temp = arr[j];
+        //             arr[j] = arr[j + 1];
+        //             arr[j+1] = temp;
+
+        //         }
+        //     }
+        // }
+        // printArr(arr);
+
+        // Selection sort
+        for(int i = 0; i < arr.length-1; i++){
+            int smallest = i;
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[smallest] > arr[j]){
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        printArr(arr);
+    }
+}
